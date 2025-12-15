541e110674d1d417870e3b9d35ec56ab6c9b374b
Catarina CSilva <catarina.silva@DB1G3515>
2025-12-15 12:22:29 -0300

    persistência de dados via TempData

--- CadastroUsuarios/Controllers/UsuarioController.cs
+++ CadastroUsuarios/Controllers/UsuarioController.cs
@@ -1,4 +1,5 @@
 ﻿using CadastroUsuarios.Models;
+using System;
 using System.Collections.Generic;
 using System.Web.Mvc;
 
@@ -8,17 +9,25 @@ namespace CadastroUsuarios.Controllers
     {
         public ActionResult Listar()
         {
-            var userList = new List<UsuarioViewModel>();
-      
-            return View(userList);
+            var listaUsuarios = RetornarUserCadastrados();
+
+            if (TempData["UsuariosNovos"] != null)
+            {
+                var listaUsuariosNovos = (List<UsuarioViewModel>)TempData["UsuariosNovos"];
+                listaUsuarios.AddRange(listaUsuariosNovos);
+            }
+
+            TempData.Keep("UsuariosNovos");
+
+            return View(listaUsuarios); //retorna view associada (mesmo nome)
         }
+
         public ActionResult Cadastrar()
         {
             var user = new UsuarioViewModel //instanciando o ViewModel
             {
                 Ativo = true //valor padrão para o campo Ativo
             };
-            
 
             return View(user);
         }
@@ -32,8 +41,35 @@ namespace CadastroUsuarios.Controllers
                 return View("Cadastrar", model);
             }
 
+            TempData["UsuariosNovos"] = RetornarNovosUsuarios(model);
             TempData["mensagemSucesso"] = "Usuário Cadastrado";  
             return RedirectToAction("Listar");
         }
+
+        private List<UsuarioViewModel> RetornarUserCadastrados()
+        {
+            return new List<UsuarioViewModel>
+            {
+                new UsuarioViewModel {Nome = "Ana", Sobrenome = "Silva", DataNascimento = new DateTime(1990, 1, 1) },
+                new UsuarioViewModel {Nome = "João", Sobrenome = "Souza", DataNascimento = new DateTime(1985, 5, 15) },
+            };
+        }
+
+        private List<UsuarioViewModel> RetornarNovosUsuarios(UsuarioViewModel model)
+        {
+            if (TempData["UsuariosNovos"] == null)
+            {
+                return new List<UsuarioViewModel>
+                {
+                    model
+                };
+            }
+            var lista = (List<UsuarioViewModel>)TempData["UsuariosNovos"];
+            lista.Add(model);
+
+            return lista;
+        }
+
     }
+
 }
